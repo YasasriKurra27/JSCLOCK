afbcfe24a2e0bdd530161d260bc3e58d922a615e
YasasriKurra27 <yasasrikurra27@gmail.com>
2025-08-28 11:56:06 +0530

    jjj

--- JSCLOCK/script.js
+++ JSCLOCK/script.js
@@ -1,7 +1,6 @@
 function updateClock() {
-    const now = new Date();
 
-    // Digital Clock
+    const now = new Date();
     const digital = document.getElementById('digital-clock');
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
@@ -12,13 +11,13 @@ function updateClock() {
     const minute = now.getMinutes();
     const hour = now.getHours();
 
-    const secondDeg = second * 6; // 360 / 60
-    const minuteDeg = minute * 6; // 360 / 60
-    const hourDeg = ((hour % 12) + minute / 60) * 30; // 360 / 12
+    const secondDeg = second * 6;
+    const minuteDeg = minute * 6;
+    const hourDeg = ((hour % 12) + minute / 60) * 30;
 
     document.getElementById('second-hand').style.transform = `rotate(${secondDeg-90}deg)`;
     document.getElementById('minute-hand').style.transform = `rotate(${minuteDeg-90}deg)`;
-    document.getElementById('hour-hand').style.transform = `rotate(${hourDeg-90      }deg)`;
+    document.getElementById('hour-hand').style.transform = `rotate(${hourDeg-90}deg)`;
 }
 
 setInterval(updateClock, 1000);
